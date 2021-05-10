f4d534f0445d66c47f8069aeea023b33451f053e
Thiago Bocchile <tykoth@gmail.com>
2021-05-10 11:44:23 -0300

    Dataprovider with conditions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import * as React from "react";
 import DebtIcon from "@material-ui/icons/Book";
 import UserIcon from "@material-ui/icons/Group";
-import { Admin, Resource, ListGuesser } from "react-admin";
-
+import { Admin, Resource } from "react-admin";
+import dataProvider from "./dataProvider";
 import { DebtList, DebtEdit, DebtCreate, DebtShow } from "./debts";
 import { UserList } from "./users";
 import Dashboard from "./Dashboard";
 import authProvider from "./authProvider";
-// import jsonServerProvider from "./dataProvider";
 
-import jsonServerProvider from "ra-data-json-server";
 const App = () => (
   <Admin
-    dataProvider={jsonServerProvider("https://jsonplaceholder.typicode.com")}
+    dataProvider={dataProvider("https://jsonplaceholder.typicode.com")}
     authProvider={authProvider}
     dashboard={Dashboard}
   >
-    <Resource name="users" icon={UserIcon} list={UserList} />
+    <Resource
+      name="dividas"
+      icon={DebtIcon}
+      list={DebtList}
+      edit={DebtEdit}
+      create={DebtCreate}
+      show={DebtShow}
+    />
+    <Resource label="Usuarios" name="users" icon={UserIcon} list={UserList} />
   </Admin>
 );
 export default App;
